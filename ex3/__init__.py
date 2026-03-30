580652113053aeded3a721173cbb8530fd3219b7
spacotto <silvia.pacotto@gmail.com>
2026-03-30 11:29:05 +0200

    Initialize package and import game engine modules
    Add package initialization and import statements for game engine components.

--- ex3/__init__.py
+++ ex3/__init__.py
@@ -1,1 +1,14 @@
+"""
+Exercise 3: Game Engine
 
+Package initialization file.
+"""
+
+from .GameStrategy import GameStrategy
+from .CardFactory import CardFactory
+from .AggressiveStrategy import AggressiveStrategy
+from .FantasyCard import FantasyCard
+from .FactoryGameEngine import FactoryGameEngine
+
+
+__all__ = []
