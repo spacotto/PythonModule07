7673a889b287ff7d8f083f36fbed8adc12349480
spacotto <silvia.pacotto@gmail.com>
2026-03-31 22:24:54 +0200

    Update Combatable.py

--- ex2/Combatable.py
+++ ex2/Combatable.py
@@ -11,10 +11,7 @@ from ex0 import Card
 
 class Combatable(ABC):
 
-    def __init__(self, name: str, cost: int, rarity: str,
-                 attack: int, health: int, defense: int) -> None:
-
-        Card.__init__(self, name, cost, rarity)
+    def __init__(self, attack: int, health: int, defense: int) -> None:
         self._attack: int = attack
         self._health: int = health
         self._defense: int = defense
