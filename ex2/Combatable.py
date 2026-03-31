2f881504bb85e9035c68302b3ebec4fa3b6884fa
spacotto <silvia.pacotto@gmail.com>
2026-03-31 18:31:00 +0200

    Update Combatable.py

--- ex2/Combatable.py
+++ ex2/Combatable.py
@@ -9,8 +9,13 @@ from typing import Dict, Any
 
 class Combatable(ABC):
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, name: str, cost: int, rarity: str,
+                 attack: int, health: int, defense: int) -> None:
+
+        Card.__init__(self, name, cost, rarity)
+        self._attack: int = attack
+        self._health: int = health
+        self._defense: int = defense
 
     @abstractmethod
     def attack(self, target: Any) -> Dict[str, Any]:
