1cec7c54f01a7ce22d8fc18bc62dff062a4b5c5b
Silvia Pacotto <spacotto@c2r8p5.42lehavre.fr>
2026-03-31 18:28:05 +0200

    Update ex4

--- ex4/main.py
+++ ex4/main.py
@@ -11,6 +11,7 @@ Demonstration script.
 
 from .TournamentPlatform import TournamentPlatform
 
+
 # ----------------------------------------------------------------------------
 #  Visual helper functions
 # ----------------------------------------------------------------------------
