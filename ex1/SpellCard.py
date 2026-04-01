519262cc5ff6fb1ecaa95f4416151ec5a2280328
spacotto <silvia.pacotto@gmail.com>
2026-04-01 14:09:01 +0200

    Update SpellCard.py

--- ex1/SpellCard.py
+++ ex1/SpellCard.py
@@ -9,6 +9,25 @@ import random
 from ex0 import Card
 
 
+# ----------------------------------------------------------------------------
+#  Spells
+# ----------------------------------------------------------------------------
+
+class Spells(Enum):
+    LIGHTNING_BOLT = ("Lightning Bolt", 3, "Common", "damage")
+    HEALING_POTION = ("Healing Potion", 2, "Common", "heal")
+    FIREBALL = ("Fireball", 4, "Uncommon", "damage")
+    SHIELD_SPELL = ("Shield Spell", 1, "Common", "buff")
+    METEOR = ("Meteor", 8, "Legendary", "damage")
+    ICE_SHARD = ("Ice Shard", 2, "Common", "damage")
+    DIVINE_LIGHT = ("Divine Light", 5, "Rare", "heal")
+    MAGIC_MISSILE = ("Magic Missile", 1, "Common", "damage")
+
+
+# ----------------------------------------------------------------------------
+#  SpellCard
+# ----------------------------------------------------------------------------
+
 class SpellCard(Card):
     """Processes instant magical effects"""
 
