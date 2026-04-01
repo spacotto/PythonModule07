3359d91b3296e5819ad3f48bd2b2a0df40b65730
spacotto <silvia.pacotto@gmail.com>
2026-04-01 14:04:39 +0200

    Update __init__.py

--- ex1/__init__.py
+++ ex1/__init__.py
@@ -4,9 +4,9 @@ Package initialization file.
 """
 
 
-from ex1.SpellCard import SpellCard
-from ex1.ArtifactCard import ArtifactCard
+from ex1.SpellCard import SpellCard, Spells
+from ex1.ArtifactCard import ArtifactCard, Artifacts
 from ex1.Deck import Deck
 
 
-__all__ = ['SpellCard', 'ArtifactCard', 'Deck']
+__all__ = ['SpellCard', 'Spells', 'ArtifactCard', 'Artifacts', 'Deck']
